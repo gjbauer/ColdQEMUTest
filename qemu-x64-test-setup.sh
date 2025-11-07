a4730595a26a3769770ff25c1e38e500c09281f9
Gabriel Bauer <gabeb1277@gmail.com>
2025-11-06 22:57:21 -0500

    some minor updates

--- qemu-x64-test-setup.sh
+++ qemu-x64-test-setup.sh
@@ -5,3 +5,5 @@ qemu-system-x86_64 -m 4G --enable-kvm -cpu host -smp 4 -hda ubuntu_vm_encrypted.
 qemu-img create -f qcow2 ubuntu_vm.qcow2 30G
 
 qemu-system-x86_64 -m 4G --enable-kvm -cpu host -smp 4 -hda ubuntu_vm.qcow2 -cdrom ~/Downloads/ubuntu*.iso -boot order=d -net nic,model=virtio -net user -display sdl,gl=on
+
+qemu-system-x86_64 -m 4G --enable-kvm -cpu host -smp 4 -hda ubuntu_vm.qcow2 -net nic,model=virtio -net user -display sdl,gl=on
