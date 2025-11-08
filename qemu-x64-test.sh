81d70193c790d62d55de475fd27e3bc40fc25ac7
Gabriel Bauer <gabeb1277@gmail.com>
2025-11-07 21:22:40 -0500

    modified test script to run cryptosift locally on host

--- qemu-x64-test.sh
+++ qemu-x64-test.sh
@@ -8,14 +8,15 @@ echo "pmemsave 0 0x20000000 dump.bin"
 qemu-system-x86_64 -m 512M --enable-kvm -cpu host -smp 4 -hda ubuntu_vm_encrypted.qcow2 -net nic,model=virtio -net user -display sdl,gl=on -monitor stdio
 
 LOOP_DEVICE=$(sudo losetup -f)
-dd if=/dev/zero of=dump.img bs=1M count=4096 status=progress
-sudo losetup "$LOOP_DEVICE" dump.img
+dd if=/dev/zero of=keys.img bs=1M count=4096 status=progress
+sudo losetup "$LOOP_DEVICE" keys.img
 sudo sgdisk -Z "$LOOP_DEVICE" && sudo sgdisk -n 0:0:0 "$LOOP_DEVICE"
 sudo partprobe "$LOOP_DEVICE"
 sudo mkfs.fat -F32 "$LOOP_DEVICE"p1
 mkdir mnt
 sudo mount -o loop "$LOOP_DEVICE"p1 mnt
-sudo cp mnt/dump.bin
+# Processing on host machine is faster than on VM...
+cryptosift -st 1 -nc dump.bin mnt/*
 sudo umount mnt
 sudo losetup -d "$LOOP_DEVICE"
 rm -rf mnt
